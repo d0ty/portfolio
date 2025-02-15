770dd0cb0837db305f1d2cf3ef1961e6fe699f16
ProgrammerPeter2 <hp.raszpi@gmail.com>
2025-02-15 21:05:01 +0100

    copied the initial layout (still lot to go)

---
+++ tailwind.config.mjs
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}", "*.{js,ts,jsx,tsx,mdx}"],
+  theme: {
+    extend: {
+      colors: {
+        background: "#121212",
+        foreground: "#ffffff",
+        muted: "#252424",
+      },
+    },
+  },
+  plugins: [],
+}
+
